d9213d3f80ff8665638d38d6cb0ee5a2f018608a
Aleksei Bodeev <bodeevaleksei@gmail.com>
2025-04-28 19:33:04 +0500

    Complete exercises 1.6-1.11

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleGoodFeedback = () => setGood(good + 1);
+  const handleNeutralFeedback = () => setNeutral(neutral + 1);
+  const handleBadFeedback = () => setBad(bad + 1);
+
+  return (
+    <div>
+      <Header title="give feedback" />
+      <Button text="good" handleClick={handleGoodFeedback} />
+      <Button text="neutral" handleClick={handleNeutralFeedback} />
+      <Button text="bad" handleClick={handleBadFeedback} />
+      <Header title="statistics" />
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  );
+};
+
+const Header = ({ title }) => <h1>{title}</h1>;
+
+const Button = ({ text, handleClick }) => (
+  <button onClick={handleClick}>{text}</button>
+);
+
+const Statistics = ({ good, neutral, bad }) => {
+  if (!good && !neutral && !bad) {
+    return <p>No feedback given</p>;
+  }
+
+  const all = good + neutral + bad;
+  const average = (good - bad) / all;
+  const positive = (good * 100) / all;
+
+  return (
+    <table>
+      <tbody>
+        <StatisticLine text="good" value={good} />
+        <StatisticLine text="neutral" value={neutral} />
+        <StatisticLine text="bad" value={bad} />
+        <StatisticLine text="all" value={all} />
+        <StatisticLine text="average" value={average} />
+        <StatisticLine text="positive" value={positive + " %"} />
+      </tbody>
+    </table>
+  );
+};
+
+const StatisticLine = ({ text, value }) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+);
+
+export default App;
